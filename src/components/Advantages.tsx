0cdfa7c3518457303dbc703e56b5c346a707e141
Andrew Mandrichuk <andrew.mandrichuk2007@gmail.com>
2024-02-19 16:56:19 +0100

    style: finalizing the 'advantages' section design v1.0

--- src/components/Advantages.tsx
+++ src/components/Advantages.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import {AdvantagesProps} from '../lib/interfaces';
-import {advantagesText} from '../constants/index';
+import images from "../constants/index";
 
-function Advantages({lang}: AdvantagesProps) {
-  const advantages = advantagesText[lang];
+import { AdvantagesProps } from "../lib/interfaces";
+import { advantagesText } from "../constants/index";
+
+import { useWindowWidth } from "../utils/useWindowWidth";
+import { toUpperCase } from "../utils/toUpperCase";
+
+function Advantages({ lang }: AdvantagesProps) {
+  const advantagesTextData = advantagesText[lang] || advantagesText["en"];
+  const windowWidth = useWindowWidth();
 
   return (
-    <div>Advantages</div>
+    <section className="advantagesSection">
+      <div className="title">{toUpperCase(advantagesTextData.title)}</div>
+      <div className="advantagesGrid">
+        {"advantages" in advantagesTextData &&
+          advantagesTextData.advantages.map((advantage, index) => (
+            <div key={`advantage-${index}`} className="advantage">
+              <div className="icon">
+                icon
+                {/* <img src={images.robbery} alt="robbery"/> */}
+              </div>
+              <div className="text">
+                <div className="subtitle">{advantage.title}</div>
+                <div className="article">{advantage.article}</div>
+              </div>
+            </div>
+          ))}
+      </div>
+      <div className="buttonContainer">
+        <button className="button accent">
+          {" "}
+          {advantagesTextData.button.name}{" "}
+        </button>
+      </div>
+    </section>
   );
 }
 
